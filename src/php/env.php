91868f79039d4a20be716e82c21d59d453e1302f
laetitiamontbarbon <laetitia.montb@hotmail.fr>
2024-04-03 11:36:36 +0200

    php marche pas

---
+++ src/php/env.php
@@ -0,0 +1,8 @@
+<?php
+echo '<pre>';
+var_dump(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_DB'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASSWORD'],
+);
--- src/php/traitement_formulaire.php
+++ src/php/traitement_formulaire.php
@@ -1,12 +1,16 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Connexion à la base de données
-$servername = "localhost"; // Remplacez par le nom de votre serveur MySQL
-$username = "votre_nom_utilisateur"; // Remplacez par votre nom d'utilisateur MySQL
-$password = "votre_mot_de_passe"; // Remplacez par votre mot de passe MySQL
-$dbname = "nom_de_votre_base_de_donnees"; // Remplacez par le nom de votre base de données
+$host =  $_ENV['DB_HOST'];  // Adresse du serveur MySQL
+$user = $_ENV['DB_USER'];  // Nom d'utilisateur MySQL
+$password = $_ENV['DB_PASSWORD'];  // Mot de passe MySQL
+$database = $_ENV['DB_DB'];  // Nom de la base de données
 
 // Création de la connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $database);
 
 // Vérifier la connexion
 if ($conn->connect_error) {
@@ -22,6 +26,9 @@ $tagNFC = $_POST['tagNFC'];
 // Requête d'insertion dans la table Utilisateur
 $sql = "INSERT INTO Utilisateur (nom, prenom, mail, clef_id, clickshare_id)
         VALUES ('$nom', '$prenom', '$mail', (SELECT clef_id FROM Clef WHERE tagNFC = '$tagNFC'), (SELECT clickshare_id FROM Clickshare WHERE tagNFC = '$tagNFC'))";
+
+// Afficher la requête SQL pour débogage
+echo "Requête SQL: $sql<br>"
 
 if ($conn->query($sql) === TRUE) {
     echo "Utilisateur ajouté avec succès.";
