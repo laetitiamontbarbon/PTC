f32a4e72f8f2196d7032eea3a3c19776bf374ad0
srtiktak <tristan.bellet69@gmail.com>
2024-04-16 10:57:30 +0200

    logs php + html.twig

---
+++ src/php/logs.php
@@ -0,0 +1,32 @@
+<?php
+
+require '../vendor/autoload.php';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=nom_de_votre_base_de_donnees', 'votre_utilisateur', 'votre_mot_de_passe');
+
+    $sql = "SELECT 
+                Pret.pret_id as id,
+                Pret.date_inscription as 'date d\'emprunt',
+                Pret.Utilisateur_id as Utilisateur,
+                Pret.clickshare_id as Clickshare,
+                Pret.clef_id as Clé,
+                Pret.rendu_check as 'rendu ?'
+            FROM 
+                Pret
+            WHERE clef_id IS NOT NULL OR clickshare_id IS NOT NULL
+            GROUP BY
+                Pret.date_inscription";
+             
+    $stmt = $dbh->query($sql);
+    $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo $twig->render('logs.html.twig', [
+        'entities' => $resultats,
+    ]);
+
+} catch (PDOException $e) {
+    echo "Échec de la connexion à la base de données : " . $e->getMessage();
+}
+
+?>
